146473b7037eda2db4263b21493dbd3679290cc1
1001josias <1001josias@gmail.com>
2024-06-23 13:33:28 -0300

    feat: Button component

--- src/env/App/App.tsx
+++ src/env/App/App.tsx
@@ -1,11 +1,18 @@
 import type { FC } from 'react'
 
-import { Counter } from '@/lib'
 import './index.css'
+import { Counter } from '@/lib'
+import Button from '@/lib/Button/'
 
 const App: FC = () => {
     return (
-        <div>
+        <div className='mt-10 ml-80'>
+            <Button variant={'default'}>Testando</Button>
+            <Button variant={'destructive'}>Testando</Button>
+            <Button variant={'secondary'}>Testando</Button>
+            <Button variant={'outline'}>Testando</Button>
+            <Button variant={'ghost'}>Testando</Button>
+            <Button variant={'link'}>Testando</Button>
             <Counter />
         </div>
     )
